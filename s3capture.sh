57a6f37b7710dc8bac59e715aec88dbdf4c9ad47
Patrick McCoy <patrick@pjmccoy.com>
2012-12-08 15:51:26 -0700

    Added date based sub directories

--- s3capture.sh
+++ s3capture.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Configurables
 FILE=/tmp/imagesnap.jpg
 BUCKET="s3://webcam.swixr.com/dev"
+
+# Date stuff
+YEAR=`date '+%Y'`
+MONTH=`date '+%m'`
+DAY=`date '+%d'`
 
 # take the photo and save it to /tmp/imagesnap.jpg
 /usr/local/bin/imagesnap $FILE
@@ -10,7 +16,7 @@ BUCKET="s3://webcam.swixr.com/dev"
 export TZ=UTC
 
 # upload the image to S3
-/usr/local/bin/s3cmd put $FILE "$BUCKET/`date '+%y-%m-%dT%H:%M:%SZ'`.jpg"
+/usr/local/bin/s3cmd put $FILE "$BUCKET/$YEAR/$MONTH/$DAY/`date '+%Y-%m-%dT%H:%M:%SZ'`.jpg"
 
 # delete the image from /tmp
 rm $FILE
