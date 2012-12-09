4fe2c58c30b15660cc497c45a0ba256dee5fcc82
Patrick McCoy <patrick@pjmccoy.com>
2012-12-08 22:15:14 -0700

    Fixed a bug where setting the TimeZone after using some date functions

--- s3capture.sh
+++ s3capture.sh
@@ -4,6 +4,9 @@
 FILE=/tmp/imagesnap.jpg
 BUCKET="s3://webcam.swixr.com/dev"
 
+# set the timezone to UTC
+export TZ=UTC
+
 # Date stuff
 YEAR=`date '+%Y'`
 MONTH=`date '+%m'`
@@ -11,9 +14,6 @@ DAY=`date '+%d'`
 
 # take the photo and save it to /tmp/imagesnap.jpg
 /usr/local/bin/imagesnap $FILE
-
-# set the timezone to UTC
-export TZ=UTC
 
 # upload the image to S3
 /usr/local/bin/s3cmd put $FILE "$BUCKET/$YEAR/$MONTH/$DAY/`date '+%Y-%m-%dT%H:%M:%SZ'`.jpg"
